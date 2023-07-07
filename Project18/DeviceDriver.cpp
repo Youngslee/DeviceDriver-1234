fa9287871ea46b82714ad9af373b5b240f5d7872
youngslee <popopo3204@naver.com>
2023-07-07 11:56:45 +0900

    [refactoring] write

--- Project18/DeviceDriver.cpp
+++ Project18/DeviceDriver.cpp
@@ -1,5 +1,15 @@
 #include "DeviceDriver.h"
 
+#include <exception>
+
+class WriteFailException : public std::exception {
+public:
+    const char* what() const throw()
+    {
+        return "WriteFailException";
+    }
+};
+
 DeviceDriver::DeviceDriver(FlashMemoryDevice* hardware) : m_hardware(hardware)
 {}
 
@@ -11,6 +21,8 @@ int DeviceDriver::read(long address)
 
 void DeviceDriver::write(long address, int data)
 {
+    int ret = read(address);
+    if (ret != 0xFF) throw* (new WriteFailException);
     // TODO: implement this method
     m_hardware->write(address, (unsigned char)data);
 }
--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -2,7 +2,44 @@
 #include "gmock/gmock.h"
 #include "../Project18/DeviceDriver.cpp"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+using namespace testing;
+using namespace std;
+class FlashMemoryDeviceMock : public FlashMemoryDevice
+{
+public:
+	MOCK_METHOD(unsigned char, read, (long address), (override));
+	MOCK_METHOD(void, write, (long address, unsigned char data), (override));
+};
+
+class DeviceDriverFixture : public testing::Test
+{
+public:
+	FlashMemoryDeviceMock flashMemoryDeviceMock;
+	void checkWriteFailException() {
+		try
+		{
+			DeviceDriver dd(&flashMemoryDeviceMock);
+			dd.write(0x2000, 100);
+			FAIL();
+		}
+		catch (WriteFailException e)
+		{
+
+		}
+	}
+};
+
+TEST_F(DeviceDriverFixture, WriteSuccessTest) {
+	EXPECT_CALL(flashMemoryDeviceMock, read(0x2000))
+		.WillRepeatedly(Return(0xFF));
+	EXPECT_CALL(flashMemoryDeviceMock, write(0x2000, 100));
+	DeviceDriver dd(&flashMemoryDeviceMock);
+	dd.write(0x2000, 100);
+}
+
+TEST_F(DeviceDriverFixture, WriteFailTest) {
+
+	EXPECT_CALL(flashMemoryDeviceMock, read(0x2000))
+		.WillRepeatedly(Return(0xAA));
+	checkWriteFailException();
 }
