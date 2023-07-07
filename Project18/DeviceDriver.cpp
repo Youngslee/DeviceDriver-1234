952cee5ed5feebf0c3b4b9243fc1719a91be2dbf
youngslee <popopo3204@naver.com>
2023-07-07 10:56:00 +0900

    [feature] readsuccess

--- Project18/DeviceDriver.cpp
+++ Project18/DeviceDriver.cpp
@@ -1,12 +1,20 @@
 #include "DeviceDriver.h"
 
+#include <exception>
+
 DeviceDriver::DeviceDriver(FlashMemoryDevice* hardware) : m_hardware(hardware)
 {}
 
 int DeviceDriver::read(long address)
 {
     // TODO: implement this method properly
-    return (int)(m_hardware->read(address));
+    int iteration = 4;
+    int ret = (int)(m_hardware->read(address));
+	while(iteration--)
+	{
+        if (ret != (int)(m_hardware->read(address))) throw std::exception("ReadFailException");
+	}
+    return ret;
 }
 
 void DeviceDriver::write(long address, int data)
--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -2,7 +2,19 @@
 #include "gmock/gmock.h"
 #include "../Project18/DeviceDriver.cpp"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+using namespace testing;
+using namespace std;
+class FlashMemoryDeviceMock : public FlashMemoryDevice
+{
+public:
+	MOCK_METHOD(unsigned char, read, (long address), (override));
+	MOCK_METHOD(void, write, (long address, unsigned char data), (override));
+};
+TEST(DeviceDriverTest, ReadSuccessTest) {
+	FlashMemoryDeviceMock flashMemoryDeviceMock;
+	EXPECT_CALL(flashMemoryDeviceMock, read(0x2000))
+		.Times(5)
+		.WillRepeatedly(Return(1000));
+	DeviceDriver dd(&flashMemoryDeviceMock);
+	cout << dd.read(0x2000) << "\n";
 }
